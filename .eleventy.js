f3bf515d56855809b8094ee019f5a50be8c209f0
Christian Engel <christian.e@schindelhauerbikes.de>
2024-05-24 09:31:22 +0200

    Minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 import CleanCSS from "clean-css";
+import htmlmin from "html-minifier";
 
 export default function (eleventyConfig) {
   // Passthrough
@@ -6,6 +7,20 @@ export default function (eleventyConfig) {
     "./static/robots.txt": "/robots.txt",
   });
 
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+
+      return minified;
+    }
+
+    // If not an HTML output, return content as-is
+    return content;
+  });
   eleventyConfig.addFilter("lowercaseFirstLetter", function (string) {
     return string.charAt(0).toLowerCase() + string.slice(1);
   });
